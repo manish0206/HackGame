3ca83a30e60527e4eb87f460de2b9e59091b6de2
manish <manish@Spare-Macbook-Pro.local>
2019-09-13 13:52:34 +0530

    init

---
+++ app/src/main/java/com/contoso/demoapp/MainActivity.java
@@ -0,0 +1,108 @@
+package com.contoso.demoapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import cn.Ragnarok.BitmapFilter;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int OVERLAY_PERMISSION_REQ_CODE = 1212;
+    double sigma=16.0;
+
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView=(ImageView) findViewById(R.id.imageView);
+        Bitmap myLogo = ((BitmapDrawable) ResourcesCompat.getDrawable(getResources(), R.drawable.ic_launcher, null)).getBitmap();
+
+        imageView.setImageBitmap(myLogo);
+        updateView(myLogo);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickImageFromGallery();
+            }
+        });
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ReactDemoActivity.openActivity(MainActivity.this);
+            }
+        });
+
+
+
+
+    }
+
+    private void updateView(Bitmap myLogo){
+        ((ImageView)findViewById(R.id.imageView1)).setImageBitmap(BitmapFilter.changeStyle(myLogo, BitmapFilter.NEON_STYLE, sigma));
+        ((ImageView)findViewById(R.id.imageView2)).setImageBitmap(BitmapFilter.changeStyle(myLogo, BitmapFilter.SKETCH_STYLE, sigma));
+        ((ImageView)findViewById(R.id.imageView3)).setImageBitmap(BitmapFilter.changeStyle(myLogo, BitmapFilter.TV_STYLE, sigma));
+        ((ImageView)findViewById(R.id.imageView4)).setImageBitmap(BitmapFilter.changeStyle(myLogo, BitmapFilter.INVERT_STYLE, sigma));
+        ((ImageView)findViewById(R.id.imageView5)).setImageBitmap(BitmapFilter.changeStyle(myLogo, BitmapFilter.BLOCK_STYLE, sigma));
+        ((ImageView)findViewById(R.id.imageView6)).setImageBitmap(BitmapFilter.changeStyle(myLogo, BitmapFilter.GOTHAM_STYLE, sigma));
+
+    }
+    private void pickImageFromGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, 101);
+    }
+    @Override
+    protected final void onActivityResult(final int requestCode,
+                                          final int resultCode, final Intent i) {
+        super.onActivityResult(requestCode, resultCode, i);
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    Toast.makeText(this, "You cannot open the React Native app as you have denied the permission", Toast.LENGTH_SHORT).show();
+                }else{
+
+                }
+            }
+        }
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case 101:
+                    manageImageFromUri(i.getData());
+                    break;
+            }
+        } else {
+            // manage result not ok !
+        }
+
+    }
+    private void manageImageFromUri(Uri imageUri) {
+        Bitmap bitmap = null;
+
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(
+                    this.getContentResolver(), imageUri);
+
+        } catch (Exception e) {
+            // Manage exception ...
+        }
+
+        if (bitmap != null) {
+            imageView.setImageBitmap(bitmap);
+
+            updateView(bitmap);
+        }
+    }
+}
---
+++ app/src/main/java/com/contoso/demoapp/ReactDemoActivity.java
@@ -0,0 +1,69 @@
+package com.contoso.demoapp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.contoso.reactnativedemolibrary.ReactNativeDemoLibrary;
+import com.facebook.soloader.SoLoader;
+
+/**
+ * Activity hosting React Native demo.
+ */
+public class ReactDemoActivity extends AppCompatActivity {
+
+    private static final int OVERLAY_PERMISSION_REQ_CODE = 9000;
+
+    public static void openActivity(Context context) {
+        Intent intent=new Intent(context,ReactDemoActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+            return;
+        }
+
+        try {
+
+            SoLoader.init(getApplication(), 0);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        openReactView();
+
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "Overlay permission denied", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            openReactView();
+        }
+    }
+
+    private void openReactView() {
+        View view = ReactNativeDemoLibrary.createHelloWorldView(this, true);
+        setContentView(view);
+    }
+}
