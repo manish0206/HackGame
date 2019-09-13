539459b68758041067b69bf2782ed5ab564e0cd9
manish <manish@Spare-Macbook-Pro.local>
2019-09-13 21:44:23 +0530

    all file commit

--- reactnativedemolibrary/index.js
+++ reactnativedemolibrary/index.js
@@ -1,50 +1,9 @@
+
 'use strict';
 
-import React from 'react';
-import {
-  AppRegistry,
-  NativeModules,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+//* @format
 
-class HelloWorld extends React.Component {
+import {AppRegistry} from 'react-native';
+import App from './App';
 
-  constructor(props) {
-    super(props);
-    this.state = {libraryName: ""};
-  }
-
-  async componentDidMount() {
-    const libraryName = await NativeModules.DemoLibrary.getName();
-    this.setState({libraryName});
-  }
-
-  render() {
-  alert(this.props);
-  console.warn(this.props)
-  console.log(this.props)
-    return (
-      <View style={styles.container}>
-        <Text style={styles.hello}>Welcome to react native! from{"\n" + this.state.libraryName}</Text>
-
-
-      </View>
-    )
-  }
-}
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  hello: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
-AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
+AppRegistry.registerComponent('HelloWorld', () => App);
